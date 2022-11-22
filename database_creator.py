58fba4b6f256e6a0eb16911917917c1aa7e936f8
Bartosz Kozlowski <kozlowski.bartosz@protonmail.com>
2022-11-22 18:48:36 +0100

    Tried to add more krasnals to db, didn't work.

--- database_creator.py
+++ database_creator.py
@@ -1,20 +1,36 @@
-import sqlite3
-#Just to create the .db file
+import csv, sqlite3
 
-connection = sqlite3.connect("resources/data/krasnale.db")
-cursor = connection.cursor()
+conn = sqlite3.connect( "resources/data/krasnale.db" )
+cur = conn.cursor()
+cur.execute('CREATE TABLE IF NOT EXISTS krasnals (x, y, name, address, author, localization)')
 
-cursor.execute("CREATE TABLE krasnals(x, y, name)")
+reader = csv.reader(open('resources/data/krasnal_db.csv', 'r'), delimiter=';')
+for field1, field2, field3, field4, field5, field6 in reader:
+    cur.execute('INSERT INTO krasnals (x, y, name, address, author, localization) VALUES (?,?,?,?,?,?)', (field1, field2, field3, field4, field5, field6))
+#data = cur.fetchall()
 
-data = [
-    (51.112158, 17.059631, "Babelek Chlaptus"),
-    (51.108669,17.065203,"Automatek"),
-]
-
-cursor.executemany("INSERT INTO krasnals VALUES(?,?,?)", data)
-connection.commit()
-
-for row in cursor.execute("SELECT * FROM krasnals"):
+for row in cur.execute("SELECT * FROM krasnals"):
     print(row)
-
-connection.close()
+    
+conn.commit()
+conn.close()    
+        
+        
+#cursor.execute("CREATE TABLE krasnals(x, y, name, address, author, localization)")
+#
+#    csv_data = csv.reader(open("resources/data/krasnal_db.csv"), delimiter=";")
+#    for rows in csv_data: # Iterate through csv
+#        cursor.execute("INSERT INTO krasnals(x, y, name, address, author, localization) VALUES (?,?,?,?,?)", *rows)
+#
+#data = [
+#    (51.112158, 17.059631, "Babelek Chlaptus"),
+#    (51.108669,17.065203,"Automatek"),
+#]
+#
+#cursor.executemany("INSERT INTO krasnals VALUES(?,?,?)", data)
+#connection.commit()
+#
+#for row in cursor.execute("SELECT * FROM krasnals"):
+#    print(row)
+#
+#connection.close()
